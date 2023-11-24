1c1b1fc82985dc0ca9369be40e2fa3b3ad5c6d68
NiekMooij <mooij.niek@gmail.com>
2023-11-24 22:41:36 +0100

    2023-11-24

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MIS',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'networkx',
+        'numpy',
+        'random',
+        'scipy',
+        'warnings',
+        'communities',
+        'pulp',
+    ],
+    entry_points={
+        'console_scripts': [
+            'continuation=MIS.continuation:main',
+            'exact=MIS.exact:main',
+            'greedy=MIS.greedy:main',
+            'lotka_volterra=MIS.lotka_volterra:main',
+        ],
+    },
+)
