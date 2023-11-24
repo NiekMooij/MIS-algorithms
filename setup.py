ef0acc85d8b19f3e1665da34528146cc02ad387e
NiekMooij <mooij.niek@gmail.com>
2023-11-24 23:09:57 +0100

    23:09

--- setup.py
+++ setup.py
@@ -7,10 +7,7 @@ setup(
     install_requires=[
         'networkx',
         'numpy',
-        'random',
         'scipy',
-        'warnings',
-        'communities',
         'pulp',
     ],
     entry_points={
